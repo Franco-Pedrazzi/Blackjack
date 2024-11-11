28ded05dae4f93166f7fa6b2be510aacadbf85ca
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-11-11 15:46:53 -0300

    solucion de error

--- BlackJack/src/App.jsx
+++ BlackJack/src/App.jsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  //const [id, setId] = useState(null)
-  const [deck, setDeck] = useState([{cards:[{image:""}, {image:""}]},{cards:[{image:""}, {image:""}]}])
+  const [deck, setDeck] = useState([]);
 
   useEffect(() => {
-
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2")
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
       .then(res => res.json())
       .then(data => {
-
-        fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`)
+        fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=4`)
           .then(res => res.json())
-          .then(data => {
-            setDeck([...deck,data])
-          })
-          console.log(deck)
-      })
-  }, [])
+          .then(drawData => {
+            setDeck(drawData.cards);
+          });
+      });
+  }, []);
 
   return (
-    <>
-      <img src={deck[0]["cards"][0]["image"]} alt="" />
-      <img src={deck[0]["cards"][1]["image"]} alt="" />
-      <br />
-      <img src={deck[1]["cards"][0]["image"]} alt="" />
-      <img src={deck[1]["cards"][1]["image"]} alt="" />
-    </>
-  )
+    <div>
+      {deck.length >= 4 ? (
+        <>
+        <center>
+        <img src={deck[0].image} alt="Card 1" />
+          <img src={deck[1].image} alt="Card 2" />
+          <br />
+          <img src={deck[2].image} alt="Card 3" />
+          <img src={deck[3].image} alt="Card 4" />
+        </center>
+
+        </>
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
