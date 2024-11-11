e9b9dea07e0c17f33923eecf56fc4aab428fbfd5
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-11-11 11:29:29 -0300

    creando segundo maso

--- BlackJack/src/App.jsx
+++ BlackJack/src/App.jsx
@@ -5,25 +5,30 @@ import './App.css'
 
 function App() {
   //const [id, setId] = useState(null)
-  const [deck, setDeck] = useState({cards:[{image:""}, {image:""}]})
+  const [deck, setDeck] = useState([{cards:[{image:""}, {image:""}]},{cards:[{image:""}, {image:""}]}])
 
   useEffect(() => {
 
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2")
       .then(res => res.json())
       .then(data => {
+
         fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`)
           .then(res => res.json())
           .then(data => {
-            setDeck(data)
+            setDeck([...deck,data])
           })
+          console.log(deck)
       })
   }, [])
 
   return (
     <>
-      <img src={deck["cards"][0]["image"]} alt="" />
-      <img src={deck["cards"][1]["image"]} alt="" />
+      <img src={deck[0]["cards"][0]["image"]} alt="" />
+      <img src={deck[0]["cards"][1]["image"]} alt="" />
+      <br />
+      <img src={deck[1]["cards"][0]["image"]} alt="" />
+      <img src={deck[1]["cards"][1]["image"]} alt="" />
     </>
   )
 }
